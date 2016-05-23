93c3822265e11165dcd3eee1284aca219568a667
Alexander Khomchenko <alexanderkh@interlink-ua.com>
2016-05-23 21:25:56 +0200

    print date of post

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,8 @@ class Article < ActiveRecord::Base
   has_attached_file :picture, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
 
   validates_attachment_content_type :picture, :content_type => /\Aimage/
+
+  def published_at
+    created_at.strftime("%Y-%m-%d")
+  end
 end
