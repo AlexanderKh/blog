abf20a5f5a8466e16673e9021c16419ac19bf7e0
Alexander Khomchenko <alexanderkh@interlink-ua.com>
2016-05-24 13:52:54 +0200

    add roles to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,14 @@
 class User < ActiveRecord::Base
-
   devise :database_authenticatable, :registerable,
          :rememberable, :validatable
+
+  enum role: { user: 0, admin: 1 }
+
+  after_initialize :set_default_role, :if => :new_record?
+
+  def set_default_role
+    self.role ||= :user
+  end
 
   # To login with username
   def email_required?
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160524110437) do
+ActiveRecord::Schema.define(version: 20160524114133) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 20160524110437) do
     t.datetime "remember_created_at"
     t.datetime "created_at",                       null: false
     t.datetime "updated_at",                       null: false
+    t.integer  "role",                default: 0,  null: false
   end
 
   add_index "users", ["username"], name: "index_users_on_username", unique: true
