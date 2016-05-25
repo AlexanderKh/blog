2c45ac2f3cc72fdf558ec9cf40602b79fbebcd37
Alexander Khomchenko <alexanderkh@interlink-ua.com>
2016-05-25 18:40:47 +0200

    write article update and delete feature spec

--- spec/features/articles_spec.rb
+++ spec/features/articles_spec.rb
@@ -16,4 +16,32 @@ feature 'Articles' do
     expect(page).to have_content 'Example'
     expect(page).to have_content 'Abra Ka Dabra'
   end
+
+  context 'Given an article' do
+    given!(:article) { create :article, user: user }
+
+    scenario 'Editing article as plain user' do
+      login_as user
+      visit '/'
+      click_link article.title
+      expect(page).to have_content article.content
+      click_link 'Edit'
+      within '.edit_article' do
+        fill_in 'Title', with: 'Example'
+        fill_in 'Content', with: 'Abra Ka Dabra'
+      end
+      click_button 'Update Article'
+      expect(page).to have_content 'Article was successfully updated'
+      expect(page).to have_content 'Example'
+      expect(page).to have_content 'Abra Ka Dabra'
+    end
+
+    scenario 'Deleting article as plain user' do
+      login_as user
+      visit '/'
+      click_link 'Delete'
+      expect(page).not_to have_content article.title
+      expect(page).to have_content 'Article was successfully destroyed'
+    end
+  end
 end
